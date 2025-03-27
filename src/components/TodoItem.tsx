57a6ca50139345d564d52cf3647ba23252816f35
Fernando Warno <ferwar@hotmail.com>
2025-03-27 15:34:44 -0300

    Pasamos las props de TodoItem

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+import { Todo } from "../types/todo";
+
+interface TodoItemProps {
+  todo: Todo;
+}
+
+export default function TodoItem({ todo }: TodoItemProps) {
+  return <div>{todo.title}</div>;
+}
