4135787dc73e4fa7b698acdac6acceb39b230ef0
Fernando Warno <ferwar@hotmail.com>
2025-03-27 22:50:08 -0300

    Creamos AddTodoForm

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,16 @@
+export default function AddTodoForm() {
+  return (
+    <form className="flex">
+      <input
+        placeholder="What needs to be done?"
+        className="rounded-s-md grow border border-gray-400 p-2"
+      />
+      <button
+        type="submit"
+        className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
