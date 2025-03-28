963e804c74a2bf65d2bc7fc5b2deac22be31ea26
Fernando Warno <ferwar@hotmail.com>
2025-03-27 23:43:11 -0300

    useState input + onSubmit

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,7 +1,27 @@
-export default function AddTodoForm() {
+import { useState } from "react";
+
+interface AddTodoFormProps {
+  onSubmit: (title: string) => void;
+}
+
+export default function AddTodoForm({ onSubmit }: AddTodoFormProps) {
+  const [input, setInput] = useState("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!input.trim()) return; // Prevent adding empty todos
+
+    onSubmit(input); // Call the onSubmit function passed as a prop
+
+    setInput(""); // Clear the input field after adding the todo
+  }
+
   return (
-    <form className="flex">
+    <form className="flex" onSubmit={handleSubmit}>
       <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         placeholder="What needs to be done?"
         className="rounded-s-md grow border border-gray-400 p-2"
       />
